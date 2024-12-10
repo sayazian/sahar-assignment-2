48ba349eed94e6bd6b45c584e01cf1238bcf9b07
sayazian <sahar.ayazian@gmail.com>
2024-12-10 09:44:58 -0800

    moved the assignment2 files to a new repo.

--- Sahar-Ayazian-Assignment1/README.md
+++
@@ -1,4 +0,0 @@
-# Assignment 1 
-
-- This is the first assignment of the Java bootcamp.
-- The topic is html and css.
---
+++ src/com/coderscampus/assignment/Assignment2.java
@@ -0,0 +1,57 @@
+package com.coderscampus.assignment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        int theNumber = createARandomNumber();
+        System.out.println("The number is " + theNumber + ".");
+        numberGuessingGame(theNumber);
+    }
+
+    private static int createARandomNumber() {
+        Random random = new Random();
+        return random.nextInt(100) + 1;
+
+    }
+    private static void numberGuessingGame(int theNumber) {
+        int numberOfGuesses = 0;
+        int guessedNumber = 0;
+        boolean userGuessedCorrectly = false;
+
+        while(numberOfGuesses < 5) {
+            guessedNumber = askForANumber();
+            numberOfGuesses++;
+            if (guessedNumber == theNumber) {
+                userGuessedCorrectly = true;
+                System.out.println("You win!");
+                break;
+            } else if (guessedNumber > theNumber) {
+                System.out.println("Please pick a lower number");
+            } else {
+                System.out.println("Please pick a higher number.");
+            }
+        }
+        if (!userGuessedCorrectly) {
+            System.out.println("You lose!\n" +
+                    "The number to guess was: " + theNumber + ".");
+        }
+
+
+    }
+
+    private static int askForANumber() {
+        Scanner input = new Scanner(System.in);
+        int theNumber = 0;
+        boolean numberInRange = false;
+        do {
+            System.out.print("Enter a number between 1 and 100: ");
+            theNumber = input.nextInt();
+            if ((theNumber >= 1)&&(theNumber <= 100)) numberInRange = true;
+            else System.out.println("Your guess is not between 1 and 100, please try again.");
+        } while(!numberInRange);
+        return theNumber;
+    }
+
+}
